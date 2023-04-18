cc64baf5a7401b2e38fcdfc420e9ecf5d12efb97
LASAA4 <LASAA4@COMSEELM-NB0618>
2023-04-18 20:25:15 +0200

    Add project files.

---
+++ Assets/Insurance.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Insurance : MonoBehaviour
+{
+    public string insuranceName;
+    public string description;
+    public int monthlyCost;
+    public int yearlyCost;
+    public Image image;
+    public Button editInsuranceButton;
+
+    public TextMeshProUGUI insuranceNameText;
+    public TextMeshProUGUI descriptionText;
+    public TextMeshProUGUI monthlyCostText;
+    public TextMeshProUGUI yearlyCostText;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/PageSwitcher.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PageSwitcher : MonoBehaviour
+{
+    public enum Page
+    {
+        ImagesPage,
+        TextPage
+    }
+    public List<GameObject> pagesList = new List<GameObject>();
+    public Page currentPage = Page.ImagesPage;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Switches page to the selected one. 
+    // Choose the selected page by choosing enum in editor
+    public void SwitchPage()
+    {
+        foreach (GameObject page in pagesList) 
+        {
+            if(page.name == currentPage.ToString())
+            {
+                print("The pressed buttons currentPage was same as the one we want to switch to, " +
+                    "therefor we do NOT set active false on it");
+                page.SetActive(true);
+            }
+            else
+                page.SetActive(false);
+        }
+    }
+}
